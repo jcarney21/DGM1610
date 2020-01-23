b7b612508f70eb595af1a423107e4203f9f33238
Jacob <jmcninja66@gmail.com>
2020-01-23 13:19:27 -0700

    Messing around with collisions

---
+++ New Unity Project/Assets/scripts/Assignments/Variables/MovingCylinder.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCylinder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Translate(0, 0, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -0.1f, 0);
+
+        transform.Translate(0, 0.2f, 0);
+    }
+
+}
