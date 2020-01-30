6a8b579a4e50381c3b4a38abf5e74900130b7464
Jacob <jmcninja66@gmail.com>
2020-01-30 12:57:52 -0700

    Spaceship controls
    W/S = Forward and backwards, Mouse = look around, Shift/space = Vertical Movement, A/D = spinning (i think)

--- Spaceship/Assets/Scripts/SpaceshipController.cs
+++ Spaceship/Assets/Scripts/SpaceshipController.cs
@@ -7,10 +7,13 @@ public class SpaceshipController : MonoBehaviour
 
     public float speed;
     public float turnSpeed;
-    public float verticalSpeed;
-    public float verticalInput;
-    public float horizontalInput;
-    public float rightInput;
+    public float spinSpeed;
+    public float jumpSpeed;
+    public float forwardInput;
+    public float spinInput;
+    public float turningInputX;
+    public float turningInputY;
+    public float jumpInput;
 
     // Start is called before the first frame update
     void Start()
@@ -21,13 +24,17 @@ public class SpaceshipController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        verticalInput = Input.GetAxis("Vertical");
-        horizontalInput = Input.GetAxis("Horizontal");
-        rightInput = Input.GetAxis("Jump");
+        forwardInput = Input.GetAxis("Vertical");
+        spinInput = Input.GetAxis("Horizontal");
+        turningInputX = Input.GetAxis("Mouse X");
+        jumpInput = Input.GetAxis("Jump");
+        turningInputY = Input.GetAxis("Mouse Y");
 
-        transform.Translate(Vector3.back * speed * Time.deltaTime * verticalInput);//Equal 2 (0, 0, .1f)
-        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
-        transform.Translate(Vector3.up * verticalSpeed * Time.deltaTime * rightInput);
+        transform.Translate(Vector3.back * speed * Time.deltaTime * forwardInput);//Equal 2 (0, 0, .1f)
+        transform.Rotate(Vector3.forward * spinSpeed * Time.deltaTime * spinInput);
+        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * turningInputX);
+        transform.Translate(Vector3.up * jumpSpeed * Time.deltaTime * jumpInput);
+        transform.Rotate(Vector3.right * turnSpeed * Time.deltaTime * turningInputY);
         // x, y, z
     }
 }
