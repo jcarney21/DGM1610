9373390bfb7d199332b94737b3bc2aecae3cc2d7
Jacob <jmcninja66@gmail.com>
2020-02-04 23:20:00 -0700

    Score Manager and Pickup

---
+++ New Unity Project/Assets/scripts/Assignments/Variables/Pickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+
+    public int pointsToAdd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider Other)
+    {
+        if (Other.gameObject.CompareTag("Player"))
+        {
+            ScoreManager.AddPoints(pointsToAdd);
+            Destroy(gameObject);
+
+        }
+
+
+    }
+}
