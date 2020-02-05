15cb66d0df4356538c9febec30fe6b9cc6a1efae
Jacob <jmcninja66@gmail.com>
2020-02-05 16:17:02 -0700

    Points in Space

---
+++ Spaceship/Assets/Scripts/Pointsystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointsystem : MonoBehaviour
+{
+
+    public int score;
+    public int pointsToAdd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Current score: " + score);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        score = score + pointsToAdd;
+        
+
+    }
+}
