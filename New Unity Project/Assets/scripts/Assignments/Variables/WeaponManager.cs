638baba5cc840c6cb072255208e2e1026585f46d
Jacob <jmcninja66@gmail.com>
2020-03-03 20:36:21 -0700

    WeaponManager
    All of the back-end functions work, except a way to pick up ammo and spawning a projectile. Don't know why. The pickup ammo doesn't work for obvious reasons (since it isn't there) but firing should work. Ill need to work on that.

--- New Unity Project/Assets/scripts/Assignments/Variables/WeaponManager.cs
+++ New Unity Project/Assets/scripts/Assignments/Variables/WeaponManager.cs
@@ -6,31 +6,166 @@ public class WeaponManager : MonoBehaviour
 {
     public float weaponDmg;
     public int magazine;
+    public int magazineMax;
     public int ammoCarrying;
     public int ammoMax;
+    public int spawnAmmo;
+    public int magazineValue;
+
+    public float reloadCycle;
+    public float reloadTime;
+
     public float fireCycle;
     public float rateOfFire;
-    public float proximityToPlayer;
 
+    public float proximityToPlayer;
+    
 
     public bool autoFire;
     public bool isActiveWeapon;
     public bool isHoldingWeapon;
-    public GameObject bullet;
+    public bool isReloading;
+    public GameObject bulletPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        magazine = magazineMax;
+        ammoCarrying = spawnAmmo;
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
+        //Data for firing weapon
+        if (isActiveWeapon && isHoldingWeapon)
+        {
+            if (magazine > 0 && fireCycle >= rateOfFire && !isReloading)
+            {
+                if (autoFire)
+                {
+                    if (Input.GetButton("Fire1"))
+                    {
+                        Instantiate(bulletPrefab, transform.position, transform.rotation);
+                        magazine -= 1;
+                        fireCycle = 0;
+
+                    }
+
+
+                }
+
+                else if (!autoFire)
+                {
+                    if (Input.GetButtonDown("Fire1"))
+                    {
+                        Instantiate(bulletPrefab, transform.position, transform.rotation);
+                        magazine -= 1;
+                        fireCycle = 0;
+
+                    }
 
 
 
+                }
+
+            }
+
+            else if (magazine == 0)
+            {
+                print("Reload");
+
+
+            }
+
+            else if (magazine == 0 && ammoCarrying == 0)
+            {
+                print("No Ammo");
+
+
+            }
+
+            else if (isReloading)
+            {
+                print("Reloading");
+
+
+
+            }
+
+
+        }
+
+        if (fireCycle < rateOfFire)
+        {
+            fireCycle += Time.deltaTime;
+
+
+
+        }
+
+        if (fireCycle > rateOfFire)
+        {
+            fireCycle = rateOfFire;
+
+
+
+        }
+
+        //Ammunition and reloading
+        if (magazine == 0 && ammoCarrying > 0)
+        {
+            isReloading = true;
+
+
+        }
+
+        if (Input.GetKeyDown("r") && ammoCarrying > 0 && magazine < magazineMax)
+        {
+            isReloading = true;
+
+
+        }
+
+        if (isReloading)
+        {
+            reloadCycle += Time.deltaTime;
+
+
+        }
+
+        if (reloadCycle >= reloadTime)
+        {
+            reloadCycle = 0;
+            magazineValue = magazine;
+            if (ammoCarrying >= magazineMax)
+            {
+                magazine = magazineMax;
+
+            }
+
+            else if (ammoCarrying < magazineMax)
+            {
+                magazine = ammoCarrying;
+
+
+            }
+            ammoCarrying = ammoCarrying - (magazineMax - magazineValue);
+            isReloading = false;
+
+
+        }
+
+        if (ammoCarrying < 0)
+        {
+            ammoCarrying = 0;
+
+
+        }
+
+        // Data to determine if you are holding a weapon
         if (isHoldingWeapon == true)
         {
             if (isActiveWeapon == false)
@@ -57,6 +192,7 @@ public class WeaponManager : MonoBehaviour
                 if (Input.GetKeyDown(KeyCode.E))
                 {
                     isHoldingWeapon = false;
+                    isActiveWeapon = false;
 
 
 
@@ -85,5 +221,10 @@ public class WeaponManager : MonoBehaviour
 
 
         }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        
     }
 }
