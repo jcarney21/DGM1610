84821476edbe6a37ea3f88458f02943a351139f3
Jacob <jmcninja66@gmail.com>
2020-03-06 19:41:04 -0700

    Pickup/drop weapon
    I also started working on a basic Hud

---
+++ New Unity Project/Assets/scripts/Assignments/Variables/DisplayText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayText : MonoBehaviour
+{
+    public Text healthText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var player = GameObject.FindWithTag("Player");
+        var health = player.GetComponent<Move>();
+        healthText.text = "Health: " + health;
+    }
+}
