cce0d6d2ffb2fc1326ffc78b45a8e8bdeb9ca601
Jacob <jmcninja66@gmail.com>
2020-03-09 17:13:01 -0600

    Basic For and while loops

---
+++ New Unity Project/Assets/scripts/Assignments/Variables/loops.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loops : MonoBehaviour
+{
+    public int numenemies = 5;
+    public int cupsInSink = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numenemies; i++)
+        {
+            Debug.Log("Enemy Number" + numenemies);
+            
+
+        }
+
+        while (cupsInSink > 0)
+        {
+            Debug.Log("Washed a Cup");
+            cupsInSink --;
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
