d031d2948490d75ed6da18278c55116efef20a1d
Jacob <jmcninja66@gmail.com>
2020-03-11 11:32:12 -0600

    Finished Grenade

---
+++ New Unity Project/Assets/scripts/Assignments/Variables/GrenadeSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeSpawner : MonoBehaviour
+{
+    public GameObject fragGrenade;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Instantiate(fragGrenade, transform.position, transform.rotation);
+
+
+        }
+
+    }
+}
