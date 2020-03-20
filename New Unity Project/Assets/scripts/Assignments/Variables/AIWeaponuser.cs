9b9288d8ede363847700f891606e28fe72c34fc0
Jacob <jmcninja66@gmail.com>
2020-03-20 12:49:25 -0600

    MoreBehaviors
    I coded a defense behavior, how to select between behaviors when it knows about the player. I also set up a hitscan system to decide when it can see the player. I just googled it and used one of the official Unity tutorials on raycasting. If it sees the player, it becomes aware of the player and triggers a combat mode (right now, I only have a defense behavior set up). If it hasn't seen the player in a while, it'll forget the player and move into its previous status, either guard or patrol.

--- New Unity Project/Assets/scripts/Assignments/Variables/AIWeaponuser.cs
+++ New Unity Project/Assets/scripts/Assignments/Variables/AIWeaponuser.cs
@@ -11,8 +11,20 @@ public class AIWeaponuser : MonoBehaviour
     public float turnSpeed;
     public float walkSpeed;
     public float patrolDistance;
+    public float maxRange;
+    public float distanceFromPlayer;
+    public float firingFrequency;
+    public float firingDuration;
+    public int agressiveness;
+    public int rotateDistance;
+    public bool walking;
+    public bool protectingALocation;
+    public float sightDistance;
+    public float fieldOfViewRange;
+    public float forgetfulness;
 
     public int status;
+    private int previousCondition;
     private int guarding = 1;
     private int patrolling = 2;
     private int movingUp = 8;
@@ -29,16 +41,13 @@ public class AIWeaponuser : MonoBehaviour
 
     public bool firing;
     public bool turning;
-    public float firingFrequency;
     public float firingCooldown;
-    public float firingDuration;
 
-    public int rotateDistance;
-    public bool walking;
+    public float awarenessCooldown;
     // Start is called before the first frame update
     void Start()
     {
-        status = attacking;
+        status = guarding;
         if (status == guarding)
         {
             InvokeRepeating("GuardingBehavior", 5.0f, 3f);
@@ -53,21 +62,90 @@ public class AIWeaponuser : MonoBehaviour
 
 
         }
+        previousCondition = status;
     }
 
     // Update is called once per frame
 
     void Update()
     {
-        if (!awareOfPlayer)
+        // for raycasting. I found it in a Unity tutorial or something
+        var player = GameObject.FindWithTag("Player");
+        RaycastHit hit;
+        //Ray lineOfSight = new Ray(transform.position, Vector3.forward);
+        Vector3 rayDirection = player.transform.position - transform.position;
+
+        /*if (Physics.Raycast(lineOfSight, out hit, sightDistance))
+        {
+            if (hit.collider.CompareTag("Player"))
+            {
+                awareOfPlayer = true;
+                canSeePlayer = true;
+                //sightCooldown = 2;
+                
+
+            }
+
+            else
+            {
+                
+
+            }
+
+
+        }*/
+
+        // all of this helps the Ai determine if it can see the player
+        if ((Vector3.Angle(rayDirection, transform.forward)) < fieldOfViewRange)
+        {
+            if (Physics.Raycast(transform.position, rayDirection, out hit, sightDistance))
+            {
+                if (hit.transform.CompareTag("Player"))
+                {
+
+                    canSeePlayer = true;
+                    awareOfPlayer = true;
+                    awarenessCooldown = forgetfulness;
+                }
+
+                else
+                {
+                    canSeePlayer = false;
+                }
+
+
+
+            }
+
+
+
+
+        }
+
+        if (!canSeePlayer && awareOfPlayer)
+        {
+            awarenessCooldown -= Time.deltaTime;
+
+
+        }
+
+        if (awarenessCooldown <= 0 && awareOfPlayer)
+        {
+            awareOfPlayer = false;
+            status = previousCondition;
+
+
+        }
+
+        if (!awareOfPlayer)// contains the behaviors for guarding and patrolling
         {
             var patrolPoint = GameObject.FindWithTag("PatrolPoint");
             patrolLocation = patrolPoint.transform;
             var proximity = Vector3.Distance(patrolLocation.position, transform.position);
             if (status == guarding && turning)
             {
-                
-                
+
+                previousCondition = guarding;
                 transform.Rotate(Vector3.up * turnSpeed * rotateDistance/5);
 
 
@@ -78,7 +156,7 @@ public class AIWeaponuser : MonoBehaviour
             {
                 if (turning)
                 {
-
+                    previousCondition = patrolling;
                     transform.Rotate(Vector3.up * turnSpeed * rotateDistance / 5);
 
 
@@ -106,9 +184,42 @@ public class AIWeaponuser : MonoBehaviour
 
 
         }
-        else if (awareOfPlayer)
+        else if (awareOfPlayer) // once the ai sees a player, this decides whether it attacks or defends
         {
-            if (status == attacking)
+            if (protectingALocation)
+            {
+                status = defending;
+
+
+
+            }
+
+            else if (!protectingALocation)
+            {
+                var playerHealth = Move.health;
+                var playerShields = Move.shields;
+                if (agressiveness > 10 || playerShields < 10)
+                {
+                    status = attacking;
+
+
+                }
+
+                else if (agressiveness < 10 && playerShields > 10)
+                {
+                    status = cautiousAttacking;
+
+
+
+
+                }
+
+
+
+
+            }
+
+            if (status == attacking) // this is for when the ai is attacking agressively
             {
                 targetingPlayer = true;
                 firing = true;
@@ -116,15 +227,89 @@ public class AIWeaponuser : MonoBehaviour
 
             }
 
-            else if (status == cautiousAttacking)
+            else if (status == cautiousAttacking) // this is for when the AI is in standard attack mode
             {
 
 
 
             }
-            else if (status == defending)
+            else if (status == defending) // this is for when the AI is in defensive mode
             {
+                if (!canSeePlayer)
+                {
+                    firing = false;
+                    targetingPlayer = false;
+                    var patrolPoint = GameObject.FindWithTag("PatrolPoint");
+                    patrolLocation = patrolPoint.transform;
+                    var proximity = Vector3.Distance(patrolLocation.position, transform.position);
 
+
+
+                    if (proximity < patrolDistance)
+                    {
+                        if (turning)
+                        {
+
+                            transform.Rotate(Vector3.up * turnSpeed * rotateDistance / 5);
+
+
+                        }
+
+                        if (walking)
+                        {
+                            transform.Translate(Vector3.forward * walkSpeed * Time.deltaTime);
+
+
+
+                        }
+
+                    }
+                    else if (proximity > (patrolDistance + 5))
+                    {
+                        transform.LookAt(patrolLocation, Vector3.up * turnSpeed);
+                        transform.Translate(Vector3.forward * walkSpeed * Time.deltaTime);
+
+
+
+                    }
+
+
+
+
+                }
+                else if (canSeePlayer)
+                {
+                    playerLocation = GameObject.FindWithTag("Player").transform;
+                    distanceFromPlayer = Vector3.Distance(playerLocation.position, transform.position);
+
+                    var patrolPoint = GameObject.FindWithTag("PatrolPoint");
+                    patrolLocation = patrolPoint.transform;
+                    var proximity = Vector3.Distance(patrolLocation.position, transform.position);
+                    if (distanceFromPlayer < maxRange)
+                    {
+                        targetingPlayer = true;
+                        firing = true;
+
+
+
+                    }
+                    else if (distanceFromPlayer > maxRange && proximity <= patrolDistance)
+                    {
+                        targetingPlayer = true;
+                        transform.Translate(Vector3.forward * walkSpeed * Time.deltaTime);
+                        firing = false;
+
+                    }
+                    else if (distanceFromPlayer > maxRange && proximity > patrolDistance)
+                    {
+                        targetingPlayer = true;
+                        firing = false;
+
+                    }
+
+
+
+                }
 
 
 
@@ -140,6 +325,8 @@ public class AIWeaponuser : MonoBehaviour
 
 
         }
+
+        // this stuff decides what weapon the ai is holding
         var weapons = GameObject.FindGameObjectsWithTag("Weapon");
         foreach (GameObject weapon in weapons)
         {
@@ -147,7 +334,7 @@ public class AIWeaponuser : MonoBehaviour
             weaponProximity = Vector3.Distance(wepLocation.position, transform.position);
             WeaponManager wm = weapon.GetComponent<WeaponManager>();
             //bool heldByEnemy = wm;
-            if (weaponProximity < 2 && firingCooldown >= firingFrequency)
+            if (weaponProximity < 2 && firingCooldown >= firingFrequency && firing)
             {
                 wm.Fire();
 
