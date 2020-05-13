8b6059c10da6b5c1c4ec7c8cc1b61ac73c8ea8e7
Jacob <jmcninja66@gmail.com>
2020-05-13 15:18:33 -0600

    Rocket Equation and thrust
    Need to work out a lot of kinks with velocity and the inputs

---
+++ Scifi experiment/Assets/Scripts/Physics/Rocket.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Thrust Calculator
+
+    public float thrust;
+
+    public float exhaustGas;
+    public float massFlowRoe;
+
+    public float acceleration; //meters/second^2
+
+    public float throttle;
+    // Rocket Equation
+    public float deltaV; // m/s^2
+
+    public float specificImpulse;
+    public float standardGravity = 9.80665f;
+
+    public float wetMass;
+    public float dryMass;
+
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+
+        // thrust and acceleration
+        thrust = exhaustGas * massFlowRoe;
+
+        acceleration = thrust / wetMass;
+
+        rb.AddRelativeForce(Vector3.forward * acceleration * throttle);
+
+        // Rocket Equation
+        deltaV = specificImpulse * standardGravity * Mathf.Log(wetMass / dryMass);
+
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            throttle = 1;
+
+
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            throttle = 0;
+
+
+        }
+    }
+}
